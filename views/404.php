00c284ccf306aea725fad3aed521f810da46ec6e
Reychelle <reychelledemonteverde.escota@bicol-u.edu.ph>
2023-05-24 09:46:52 +0800

    style 404

--- views/404.php
+++ views/404.php
@@ -1,1 +1,4 @@
-<h1>Page not Found!</h1>
+<?php require "views/partials/header.php"; ?>
+<div class=" d-grid" style="height:100vh; place-items:center">
+    <h1 class=" text-white text-center">404 <br>Page not found</h1>
+</div>
